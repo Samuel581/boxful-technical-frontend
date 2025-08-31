6eb22f7d5b47258a97466d577139825dbe955f23
samuel <00023020@uca.edu.sv>
2025-08-31 00:22:21 -0600

    :art: Improve user service with types and DTOs

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,10 @@
 import { api } from "@/lib/api/axios";
+import { UserProfileResponseDto } from "@/lib/dtos/user.dto";
+import {User} from "@/types/user"
 
 export const usersService = {
-    async profile(){
-        const user = api.get('/users/profile')
+    async profile(): Promise<User>{
+        const response = await api.get<UserProfileResponseDto>('/users/profile')
+        return response.data;
     }
 }

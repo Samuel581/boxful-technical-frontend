7212c1c23452f4126e3ae25fbf029ab75b85477e
samuel <00023020@uca.edu.sv>
2025-08-31 00:21:43 -0600

    :art: Improve auth service with types and DTOs

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,15 +1,16 @@
 import { api } from "@/lib/api/axios";
 import { Login } from "@/types/login";
 import { Register } from "@/types/register";
+import {RegisterRequestDto, LoginRequestDto, AuthResponseDto } from"@/lib/dtos/auth.dto";
 
 export const authService = {
-    async register(){
-        const response = await api.post<Register>('/auth/register')
+    async register(data: RegisterRequestDto): Promise<Omit<Register, 'password'>>{
+        const response = await api.post<AuthResponseDto>('/auth/register', data)
         return response.data
     },
 
-    async login(){
-        const response = await api.post<Login>('/auth/login')
+    async login(data: LoginRequestDto): Promise<AuthResponseDto>{
+        const response = await api.post<AuthResponseDto>('/auth/login', data)
         return response.data
     }
 }

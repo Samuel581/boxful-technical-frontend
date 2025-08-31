243c2dc7c6cef20504b4e2574c935f443561bd03
samuel <00023020@uca.edu.sv>
2025-08-30 19:57:50 -0600

    :sparkles: Implement add products to order form

--- src/components/orders/OrdersForm/ProductsInfoStep.tsx
+++ src/components/orders/OrdersForm/ProductsInfoStep.tsx
@@ -1,25 +1,62 @@
 import React from "react";
-import { Row, Col, Form, Input } from "antd";
+import { Row, Col, Form, Input, Typography, Button } from "antd";
+import { Plus } from "lucide-react";
 function ProductsInfoStep() {
   return (
-    <div className="flex flex-col">
-      <Row gutter={16}>
-        <Col span={8}>
-          <Form.Item label="First Name">
-            <Input placeholder="Enter first name" />
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item label="Last Name">
-            <Input placeholder="Enter last name" />
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item label="Email">
-            <Input placeholder="Enter email" />
-          </Form.Item>
-        </Col>
-      </Row>
+    <div className="flex flex-col gap-4">
+      <p className="font-bold text-xl">Agrega tus productos</p>
+      <div className="flex flex-row gap-10 justify-evenly">
+        <div className="flex flex-row w-2/5">
+          <div>
+            <Typography className="font-bold">
+              Largo
+            </Typography>
+            <Input
+              placeholder="Ingresa la direccion de recoleccion"
+              className="w-full"
+            ></Input>
+          </div>
+          <div>
+            <Typography className="font-bold">
+              Alto
+            </Typography>
+            <Input
+              placeholder="Ingresa la direccion de recoleccion"
+              className="w-full"
+            ></Input>
+          </div>
+          <div>
+            <Typography className="font-bold">
+              Ancho
+            </Typography>
+            <Input
+              placeholder="Ingresa la direccion de recoleccion"
+              className="w-full"
+            ></Input>
+          </div>
+        </div>
+        <div className="w-1/5">
+            <Typography className="font-bold">
+              Peso en libras
+            </Typography>
+            <Input
+              placeholder="Ingresa la direccion de recoleccion"
+              className="w-full"
+            ></Input>
+          </div>
+          <div className="w-2/5">
+            <Typography className="font-bold">
+              Contenido
+            </Typography>
+            <Input
+              placeholder="Ingresa la direccion de recoleccion"
+              className="w-full"
+            ></Input>
+          </div>
+      </div>
+      <div className="flex justify-end mt-4">
+        <Button className="w-1/6">Agregar <Plus/></Button>
+      </div>
     </div>
   );
 }

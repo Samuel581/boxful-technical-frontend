b4936a77815044ae8c48f5171d2979ecf70ee225
samuel <00023020@uca.edu.sv>
2025-08-30 19:28:40 -0600

    :sparkles: Add top banner

---
+++ src/components/orders/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='h-1/8 bg-gray-200 w-full flex items-center'>
+      <div className='flex flex-row justify-between w-full px-5'>
+        <b>Crear orden</b>
+        <b>Tu nombre</b>
+      </div>
+    </div>
+  )
+}
+
+export default Banner

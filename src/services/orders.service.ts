8dbb9d81188de29ca51aca89d45d6ac452c9af42
samuel <00023020@uca.edu.sv>
2025-08-31 00:22:02 -0600

    :art: Improve orders service with types and DTOs

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,13 +1,14 @@
 import { api } from "@/lib/api/axios";
 import { Order } from "@/types/order";
+import { CreateOrderRequestDto, OrderResponseDto } from "@/lib/dtos/order.dto"
 
 export const ordersService = {
-    async create(){
-        const order = api.post<Order>('orders')
-        return order;
+    async create(data: CreateOrderRequestDto): Promise<Omit<Order, 'userId'>>{
+        const response = await api.post<OrderResponseDto>('orders', data)
+        return response.data;
     },
-    async getAll(){
-        const orders = api.post<Order[]>('orders')
-        return orders;
+    async getAll(): Promise<Omit<Order, 'userId'>[]>{
+        const response = await api.get<OrderResponseDto[]>('orders')
+        return response.data;
     }
 }

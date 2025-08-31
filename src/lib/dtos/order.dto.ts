58743ed3c0b425d0033a214bb6af48a21675f49e
samuel <00023020@uca.edu.sv>
2025-08-31 00:20:09 -0600

    :sparkles: Add order DTO

---
+++ src/lib/dtos/order.dto.ts
@@ -0,0 +1,28 @@
+export interface CreateOrderRequestDto {
+    collectionAddress: string;
+    destinationAddress: string;
+    destinationFirstName: string;
+    destinationLastName: string;
+    destinationEmail: string;
+    destinationPhone: string;
+    department: string;
+    province: string;
+    reference: string;
+    addressReference: string;
+    additionalNotes?: string;
+    scheduledDate: string;
+    products: ProductDataDto[];
+  }
+  
+  export interface ProductDataDto {
+    name: string;
+    weight: number;
+    length: number;
+    height: number;
+    width: number;
+  }
+  
+  // DRY
+  export interface OrderResponseDto extends CreateOrderRequestDto {
+    id: string
+  }

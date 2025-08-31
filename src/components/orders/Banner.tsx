fede00c29c5fc4a535c05726bde07551789418db
samuel <00023020@uca.edu.sv>
2025-08-31 07:02:35 -0600

    :sparkles: Add dynamic message behavior

--- src/components/orders/Banner.tsx
+++ src/components/orders/Banner.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+"use client"
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
 function Banner() {
+  const pathname = usePathname()
+  const [bannerMessage, setBannerMessage] = useState("")
+
+  useEffect(() => {
+    if (pathname.includes("/create-order")) {
+      setBannerMessage("Crear orden");
+    } else if (pathname.includes("/history")) {
+      setBannerMessage("Mis envios");
+    }
+  }, [pathname]);
   return (
     <div className='h-1/8 bg-gray-200 w-full flex items-center'>
       <div className='flex flex-row justify-between w-full px-5'>
-        <b>Crear orden</b>
+        <b>{bannerMessage}</b>
         <b>Tu nombre</b>
       </div>
     </div>
